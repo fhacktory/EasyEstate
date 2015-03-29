67d23b8165104a40d1525094e56a2d23738158c1
Yann Vaillant <yann@vaillant.im>
2015-03-29 03:14:46 +0200

    angular ca marche pas

--- src/javascript/controller/index.js
+++ src/javascript/controller/index.js
@@ -1,7 +1,12 @@
-app.controller("IndexCtrl", function($scope, $firebaseObject) {
-  var settings = new Firebase("https://fiery-fire-2189.firebaseio.com/settings");
-  /* var adverts = new Firebase("https://fiery-fire-2189.firebaseio.com/adverts");
-  var amenities = new Firebase("https://fiery-fire-2189.firebaseio.com/amenities"); */
+app.controller("IndexCtrl", function($scope, $firebaseArray) {
+  var settings = new Firebase("//fiery-fire-2189.firebaseio.com/settings");
+  var adverts = new Firebase("//fiery-fire-2189.firebaseio.com/adverts");
+  var amenities = new Firebase("//fiery-fire-2189.firebaseio.com/amenities");
+
+  $scope.settings = $firebaseArray(settings);
+
+  $scope.limited_adverts = $firebaseArray(adverts.limitToLast(25));
+  console.log($scope.limited_adverts)
 
   $scope.data = {};
 
@@ -14,7 +19,6 @@ app.controller("IndexCtrl", function($scope, $firebaseObject) {
       attribution: '&copy; <a href="//osm.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 18
     }).addTo($scope.map);
-    
-    $scope.settings = $firebaseObject(settings);
+
   })();
 });
