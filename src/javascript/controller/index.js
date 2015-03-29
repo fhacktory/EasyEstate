40b2942123582f2f22c50f97e48b73781fb555bb
Florent Espanet <florent.esp@gmail.com>
2015-03-29 10:19:40 +0200

    Add price.

--- src/javascript/controller/index.js
+++ src/javascript/controller/index.js
@@ -6,6 +6,7 @@ app.controller("IndexCtrl", function($scope, $firebaseObject, $firebaseArray, $t
 
   $scope.checked = {};
   $scope.best_districts = [];
+  $scope.prices = {};
 
   adverts.on("value", function(snapshot) {
     $timeout(function() {
@@ -20,16 +21,32 @@ app.controller("IndexCtrl", function($scope, $firebaseObject, $firebaseArray, $t
   };
 
   $scope.sizeToInt = function(size) {
-    console.log(size);
     return parseInt(size.replace(/\s+/g, '').split('m')[0]);
   };
 
   $scope.addLayer = function(osm_key) {
-    var amenities_number = $scope.amenities_number[osm_key];
+    var amenities_number = {};
+    if (osm_key == 'price') {
+      amenities_number = $scope.prices;
+    } else {
+      amenities_number = $scope.amenities_number[osm_key];
+    }
+
+    var make_color = function(current) {
+      var max = amenities_number[$scope.best_districts[0]];
+      var min = amenities_number[$scope.best_districts[$scope.best_districts.length - 1]];
+
+      if (min === undefined) { min = 0; }
+      if (max === undefined) { max = 0; }
+      if (current === undefined) { current = 0; }
+      console.log(current , max, min, current * 255 / (max - min));
+      return (current - min) * 255 / (max - min);
+    };
+
     for (var key in $scope.amenities) {
       var amenity = $scope.amenities[key];
       layers.addLayer(L.geoJson(amenity.polygon, {
-        color: "rgb(75, "+ amenities_number[key]  +", 0)",
+        color: "rgb(75, "+ make_color(amenities_number[key]) +", 0)",
         weight: 5,
         fillOpacity: 0.90,
         opacity: 0.90,
@@ -38,7 +55,12 @@ app.controller("IndexCtrl", function($scope, $firebaseObject, $firebaseArray, $t
   };
 
   $scope.setBestDistrict = function(osm_key) {
-    var amenities_number = $scope.amenities_number[osm_key];
+    var amenities_number = {};
+    if (osm_key == "price") {
+      amenities_number = $scope.prices;
+    } else {
+      amenities_number = $scope.amenities_number[osm_key];
+    }
     $scope.best_districts = Object.keys(amenities_number).sort(function(a, b) {
       return amenities_number[b] - amenities_number[a];
     });
@@ -49,6 +71,7 @@ app.controller("IndexCtrl", function($scope, $firebaseObject, $firebaseArray, $t
       $scope.amenities = snapshot.val();
       for (var key in snapshot.val()) {
         var amenity = snapshot.val()[key];
+        $scope.prices[key] = amenity.price;
         for (var node in amenity.nodes) {
           if (!$scope.amenities_number[node]) {
             $scope.amenities_number[node] = {};
@@ -72,8 +95,8 @@ app.controller("IndexCtrl", function($scope, $firebaseObject, $firebaseArray, $t
     $scope.best_districts = [];
     layers.clearLayers();
     if ($scope.checked[osm_key]) {
-      $scope.addLayer(osm_key);
       $scope.setBestDistrict(osm_key);
+      $scope.addLayer(osm_key);
     }
   };
 
